122b30c33c6f87b4a84ecd87c068972a172e3966
Andrew Pittman <andrew@pittman.dev>
2020-03-08 18:41:53 -0400

    bunch of updates

---
+++ src/components/ImageDetail.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Text, StyleSheet, View, Image } from 'react-native'
+
+const ImageDetail = ({ title, imageSource, score }) => {
+  return (
+    <View>
+      <Image source={imageSource} />
+      <Text>
+        {title}: Score of {score}
+      </Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
+
+export default ImageDetail
