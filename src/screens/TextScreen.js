f47ec6e0dd97ce130d1b36a55cc357aa88b2adc7
Andrew Pittman <andrew@pittman.dev>
2020-03-08 20:03:50 -0400

    boiler textinput

---
+++ src/screens/TextScreen.js
@@ -0,0 +1,20 @@
+import React, { useReducer } from 'react'
+import { Text, StyleSheet, View, TextInput } from 'react-native'
+
+const TextScreen = () => {
+  return (
+    <View>
+      <TextInput style={styles.input} />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  input: {
+    margin: 15,
+    borderColor: 'black',
+    borderWidth: 1
+  }
+})
+
+export default TextScreen
